842d8e2bc0b8e96490c5352d6ddde945cc201677
Ali Shah <s.ashah@outlook.com>
2021-03-16 21:43:20 -0400

    [Update] split search regex into tag/amount

--- app.py
+++ app.py
@@ -1,25 +1,39 @@
 # %%
 import re
 import pdfplumber
+from collections import namedtuple
 
 # %%
-# For multiple pages:
+Transaction = namedtuple('Transaction', ['tag', 'amount'])
+
+# %%
+example_str = '01/25/21 01/20/21 00408951196 NYSTA 25 01/20/21 24 01/20/21 23:50 BUSINESS 5H $1.35 $128.42'
+
+# %%
+tag_re = re.compile(r'\d{11}')
+amount_re = re.compile(r'([\d,]+\.\d{2} )')
+
+# %%
+tag = tag_re.search(example_str)
+amount = amount_re.search(example_str)
+
+# %%
+t1 = Transaction(tag, amount)
+print(t1.tag)
+
+# %% For multiple pages:
 # with pdfplumber.open("misc/examples/statement_02_25_2021.pdf") as pdf:
 #     pages = pdf.pages
 #     for page in pdf.pages:
 #         texts = [page.extract_text() for page in pages]
 
-# %%
-# For a single page:
+# %% For a single page:
 with pdfplumber.open("misc/examples/statement_02_25_2021.pdf") as pdf:
     page = pdf.pages[0]
     text = page.extract_text()
 
 # %%
 lines = text.split('\n')
-
-# %%
-filter = re.compile(r'^((\d{2}\/){2}\d{2} )+')
 
 # %%
 for line in lines:
