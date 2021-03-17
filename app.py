65eeb3af2cf48a570660c23126c8b7d84a5b9dea
Ali Shah <s.ashah@outlook.com>
2021-03-17 00:20:56 -0400

    [Update] export to csv

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 # %%
 import re
 import pdfplumber
+from pandas import DataFrame
 from collections import namedtuple
 
 # %%
 Transaction = namedtuple('Transaction', ['tag', 'amount'])
 
 # %%
-example_str = '01/25/21 01/20/21 00408951196 NYSTA 25 01/20/21 24 01/20/21 23:50 BUSINESS 5H $1.35 $128.42'
-
-# %%
 tag_re = re.compile(r'\d{11} ')
 amount_re = re.compile(r'([\d,]+\.\d{2} )')
+
+# %%
+example_str = '01/25/21 01/20/21 00408951196 NYSTA 25 01/20/21 24 01/20/21 23:50 BUSINESS 5H $1.35 $128.42'
 
 # %%
 tag = tag_re.search(example_str).group()
@@ -42,3 +43,7 @@ for _ in range(len(transactions)):
     amount += transactions[_].amount
 
 print(round(amount, 2))
+
+# %%
+df = DataFrame(transactions)
+df.to_csv('results.csv', index=False)
