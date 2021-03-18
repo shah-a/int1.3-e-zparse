b0b9f840728bf4dc763f2a3f29c99ab4b3186462
Ali Shah <s.ashah@outlook.com>
2021-03-18 06:18:14 -0400

    [Update] optimized script and made it modular

--- app.py
+++ app.py
@@ -4,48 +4,54 @@ import pdfplumber
 from pandas import DataFrame
 from collections import namedtuple
 
-# %%
-Transaction = namedtuple('Transaction', ['tag', 'amount'])
+def load_pages():
+    invoice = "misc/examples/statement_02_25_2021.pdf"
+    with pdfplumber.open(invoice) as pdf:
+        return [page.extract_text() for page in pdf.pages]
+
+def transactions(pages, search_term):
+    Transaction = namedtuple('Transaction', ['tag', 'amount'])
+
+    # tag_re = re.compile(r'\d{11} ')  # This line determines what the script searches for
+    tag_re = re.compile(search_term)
+    amount_re = re.compile(r'([\d,]+\.\d{2} )')
+
+    transactions = []
+
+    for page in pages:
+        for line in page.split('\n'):
+            tag = tag_re.search(line)
+            amount = amount_re.search(line)
+            if tag and amount:
+                tag = tag.group().strip()
+                amount = float(amount.group().replace(',', ''))
+                transactions.append(Transaction(tag, amount))
+
+    return transactions
 
 # %%
-tag_re = re.compile(r'\d{11} ')
-amount_re = re.compile(r'([\d,]+\.\d{2} )')
+pages = load_pages()
 
 # %%
-# example_str = '01/25/21 01/20/21 00408951196 NYSTA 25 01/20/21 24 01/20/21 23:50 BUSINESS 5H $1.35 $128.42'
+tags = [
+    '00406214663',
+    '00406293975',
+    '00407040487',
+    '00407041779',
+    '00408951196',
+    '00409181239',
+    'Lease Tag'
+]
 
-# # %%
-# tag = tag_re.search(example_str).group()
-# amount = amount_re.search(example_str).group()
+# %% for individual outputs:
+data_frames = []
+for tag in tags:
+    tag_transactions = transactions(pages, tag)
+    df = DataFrame(tag_transactions)
+    data_frames.append(df)
+    df.to_csv(f'outputs/transactions_{tag}.csv', index=False)
 
-# print(tag)
-# print(amount)
-
-# %%
-file = "misc/examples/statement_02_25_2021.pdf"
-with pdfplumber.open(file) as pdf:
-    pages = [page.extract_text() for page in pdf.pages]
-
-
-# %%
-transactions = []
-
-for page in pages:
-    for line in page.split('\n'):
-        tag = tag_re.search(line)
-        amount = amount_re.search(line)
-        if tag and amount:
-            tag = tag.group().strip()
-            amount = float(amount.group().replace(',', ''))
-            transactions.append(Transaction(tag, amount))
-
-amount = 0
-
-for _ in range(len(transactions)):
-    amount += transactions[_].amount
-
-print(round(amount, 2))
-
-# %%
-df = DataFrame(transactions)
-df.to_csv('results.csv', index=False)
+# %% For combined output:
+output = transactions(pages, r'\d{11} ')
+df = DataFrame(output)
+df.to_csv('outputs/combined.csv', index=False)
