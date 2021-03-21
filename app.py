af09d69fc52453c2a492e518bda7fe1129dc3eef
Ali Shah <s.ashah@outlook.com>
2021-03-20 23:08:56 -0400

    [Update] experimenting with form for tags

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config['ENV'] = 'development'
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        tags = request.form.get('tags')
+        tags = tags.split(',')
+        tags = [tag.strip() for tag in tags]
+        print(tags)
+        return render_template('index.html')
+
     return render_template('index.html')
 
 if __name__ == '__main__':
